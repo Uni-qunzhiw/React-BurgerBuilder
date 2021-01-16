686d9ceec344da1bb5be3389ac2555320f7e65cb
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2021-01-16 15:55:08 +1100

    add/remove burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,24 @@ import classes from '../Burger/Burger.module.css';
 
 const burger = (props) => {
    // convert state object (ingridients) to an array;
-    const transformedIngredietns = Object.keys(props.ingredients)
+    let transformedIngredietns = Object.keys(props.ingredients)
         .map( igKey => {
             // underscore sign _ used in map method represents an unused param
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredients key={igKey + i} type = {igKey} />
-        });
-    });
-    console.log(transformedIngredietns);
+            });
+        })
+        .reduce((arr, el) => {    
+            return arr.concat(el)
+        }, []);
+        // reduce function take the accumulator (arr) and concate it with current value (el) 
+        // given initial value provided (empty array in this case)
+        // therefore arr is [], and it concat with first elemt el in the array
+        // in first round
+
+    if(transformedIngredietns.length === 0) {
+        transformedIngredietns = <p> Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type = "bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Aux from "../../hoc/Aux";
+
+const INGRREDIENT_PRICE = {
+    salad: 1,
+    cheese: 1,
+    meat: 3,
+    bacon: 2
+}
 
 class BurgerBuilder extends Component {
 
@@ -12,20 +20,63 @@ class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
-        }
-    }
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 9.85
+    };
 
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        // update in immutable way;
+        const updatedIngredients = {
+           ...this.state.ingredients 
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceAddition = INGRREDIENT_PRICE[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    };
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if( oldCount <= 0) {
+            return;
+        }
+        const updatedCounted = oldCount - 1;
+        // update in immutable way;
+        const updatedIngredients = {
+           ...this.state.ingredients 
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceDeduction = INGRREDIENT_PRICE[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    };
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        for( let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <Aux>
 
                 <div>Build Controls</div>
                 <Burger ingredients = {this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded = {this.addIngredientHandler}
+                    ingredientDeleted = {this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
