2e5bd5324f4d400276b4235784315ac979dcc6c1
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2021-01-17 13:11:32 +1100

    improve performance
    by stop updating order summary in modal while add burger ingredients

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,36 @@
-import React from'react';
+import React , {Component} from'react';
 
 
 import Aux from '../../../hoc/Aux';
 import Button from'../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    
+    componentDidUpdate() {
+        console.log("[Order Summary] Did update");
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style = {{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style = {{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         });
-        console.log(ingredientSummary);
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-               {ingredientSummary} 
-            </ul>
-            <p><strong>Total Prcie: {props.summary} </strong></p>
-            <p>continue to checkout?</p>
-            <Button btnType = 'Danger' clicked = {props.purchaseCanceled}>Cancel</Button>
-            <Button btnType = 'Success' clicked = {props.purchaseContinue}>Continue</Button>
-        </Aux>
-    )
-};
-
-export default orderSummary;
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                {ingredientSummary} 
+                </ul>
+                <p><strong>Total Prcie: {this.props.summary} </strong></p>
+                <p>continue to checkout?</p>
+                <Button btnType = 'Danger' clicked = {this.props.purchaseCanceled}>Cancel</Button>
+                <Button btnType = 'Success' clicked = {this.props.purchaseContinue}>Continue</Button>
+            </Aux>
+        )
+    };
+}
+export default OrderSummary;
