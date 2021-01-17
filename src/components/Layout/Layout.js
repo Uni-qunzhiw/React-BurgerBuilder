9017a497e3c338f025e7b6baaed04ec718c0b141
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2021-01-17 12:49:27 +1100

    add tiggle button for side drawer and responsive adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,39 @@
 // layout can wrap another component around
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from '../Layout/Layout.module.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/ToolBar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    // to handle adjecnet jsx elements, 
-    // frist: we could return them in to an array
-    // second: we can use higher order components OR react.fragment
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className = {classes.Content}>
-            {/* output components wrapped with this layout. */}
-            {props.children}
-        </main>
-    </Aux>
-    
-);
+class Layout extends Component{
 
-export default layout;
+    state = {
+        showSideDrawer : false
+    }
+
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false})
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer : !prevState.showSideDrawer}})
+    };
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar DrawerToggleClicked = {this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open = {this.state.showSideDrawer}
+                    closed = {this.sideDrawerClosedHandler}/>
+                <main className = {classes.Content}>
+                    {/* output components wrapped with this layout. */}
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
+
+export default Layout;
