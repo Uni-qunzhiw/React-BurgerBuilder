da5997ecf86dc0c9794ed96af95e06db131abdcd
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2021-01-17 11:57:55 +1100

    add a side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,12 +4,14 @@ import React from 'react';
 import classes from '../Layout/Layout.module.css';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/ToolBar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = (props) => (
     // to handle adjecnet jsx elements, 
     // frist: we could return them in to an array
     // second: we can use higher order components OR react.fragment
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className = {classes.Content}>
             {/* output components wrapped with this layout. */}
             {props.children}
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+const navigationItmes = () => (
+    <ul className = {classes.NavigationItems}>
+        <NavigationItem link= "/" active> Burger Builder</NavigationItem>
+        <NavigationItem link = "/"> Burger Builder</NavigationItem>
+    </ul>
+);
+
+export default navigationItmes;
