67fbaafa2e398eb083646672f4a3a1fbbf49a616
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2021-01-17 17:55:56 +1100

    add order summary route & prefix root route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout> 
-          <BurgerBuilder/>
-          <Checkout  />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            {/* with this order and exact, it is not treated as prefix thus loaded multiple times */}
+            <Route path="/" exact component={BurgerBuilder} /> 
+          </Switch>
         </Layout>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+
+ReactDOM.render(app ,document.getElementById('root')
 );
 
 
