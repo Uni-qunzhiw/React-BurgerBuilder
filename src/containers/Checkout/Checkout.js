cd8482f8f663101a4ec00eae15c13026d93479bd
qunzhi wang <adanwzy@gmail.com>
2021-01-21 12:20:44 +1100

    test

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,42 +1,44 @@
-import React, {Component}  from 'react';
+import React, { Component } from "react";
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
-class Checkout extends Component  {
-    state = {
-        ingredients: {
-            cheese: 1,
-            meat: 1,
-            salad: 1,
-            bacon: 1
-        }
-    };
+class Checkout extends Component {
+  // test
+  state = {
+    ingredients: {
+      cheese: 1,
+      meat: 1,
+      salad: 1,
+      bacon: 1,
+    },
+  };
 
-    compoennetDidMout() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
-        }
-        this.setState({ingredients : ingredients});
+  compoennetDidMout() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
     }
-    checkoutCancelled = () => {
-        this.props.history.goBack();
-    }
-    checkoutContinued = () => {
-        this.props.history.replace('/checkout/contact-data');
-    }
+    this.setState({ ingredients: ingredients });
+  }
+  checkoutCancelled = () => {
+    this.props.history.goBack();
+  };
+  checkoutContinued = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
 
-    render () {
-        return (
-            <div>
-                <CheckoutSummary 
-                    checkoutCancelled = {this.checkoutCancelled}
-                    checkoutContinued = {this.checkoutContinued}
-                    ingredients = {this.state.ingredients}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <CheckoutSummary
+          checkoutCancelled={this.checkoutCancelled}
+          checkoutContinued={this.checkoutContinued}
+          ingredients={this.state.ingredients}
+        />
+      </div>
+    );
+  }
 }
 
 export default Checkout;
